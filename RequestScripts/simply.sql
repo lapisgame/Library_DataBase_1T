0fc8a0cc67cb7fe919def97f25ae2e27ea88ba92
lapis <klimantovartem@gmail.com>
2023-07-10 01:02:44 +0300

    3-8 simply requests

--- RequestScripts/simply.sql
+++ RequestScripts/simply.sql
@@ -28,20 +28,57 @@ JOIN public.reader r on e.reader_ticket_number = r.reader_ticket_number
 GROUP BY r.reader_ticket_number 
 ORDER BY r.reader_ticket_number 
 
-
 /*Определить читателей, у которых на руках определенная книга.*/
-
+SELECT 
+	e.reader_ticket_number AS Номер_Билета, 
+	r.r_surname AS Фамилия, 
+	r.r_name AS Имя, 
+	r.r_patronymic AS Отчество, 
+	b.name AS Название_книги
+FROM extradition e
+JOIN reader r ON e.reader_ticket_number = r.reader_ticket_number
+JOIN book b ON e.book_id = b.book_id 
+WHERE ((return_date IS NULL) AND (real_return_date IS NULL) AND e.book_id = 0005)
 
 /*Определите, какие книги на руках читателей.*/
-
+SELECT 
+	e.extradition_id AS Номер_выдачи, 
+	b.name AS Название_книги
+FROM extradition e
+JOIN book b ON e.book_id = b.book_id 
+WHERE ((return_date IS NULL) AND (real_return_date IS NULL))
 
 /*Вывести количество должников на текущую дату. */
-
+SELECT 
+	COUNT(CASE
+		WHEN ((e.return_date IS NULL) AND (e.real_return_date IS NULL) AND (e.date_of_issue < (current_date - 14))) THEN 1
+		ELSE NULL
+	END) AS Должники
+FROM extradition e 
 
 /*Книги какого издательства были самыми востребованными у читателей? Отсортируйте издательства по убыванию востребованности книг.*/
-
+SELECT 
+	b.publisher AS Код_издательства, 
+	COUNT(*) as Количество_книг
+FROM extradition e
+JOIN book b ON e.book_id = b.book_id  
+GROUP BY publisher
+ORDER BY Количество_книг DESC
 
 /*Определить самого издаваемого автора.*/
-
+SELECT 
+	author, 
+	COUNT(*) as Книги_автора
+FROM book
+GROUP BY author
+ORDER BY Книги_автора DESC
+LIMIT 1
 
 /*Определить среднее количество прочитанных страниц читателем за день.*/
+SELECT e.extradition_id, e.date_of_issue, e.real_return_date, CASE 
+	WHEN ((e.return_date IS NOT NULL) AND (e.real_return_date IS NOT NULL)) 
+	THEN (b.volume_page * 1.0 / (e.real_return_date - e.date_of_issue))
+	ELSE NULL
+END AS AVG_страниц
+FROM extradition e 
+JOIN book b ON e.book_id = b.book_id 
